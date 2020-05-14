bb641d20742b392a4f9174bbdf4bba6d871b7e5a
Todor Vlasev <t.vlasev@gmail.com>
2020-05-14 22:03:20 +0300

    add logic for addding, removing, listing and reading a note

--- app.js
+++ app.js
@@ -1,8 +1,49 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 
-const command = process.argv[2];
+yargs.command({
+    command: 'add',
+    describe: "Add a new note",
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log("Title: " + argv.title, " Body: " + argv.body);
+    }
+})
 
-if(command === 'add'){
-    console.log("adding node!");
-}
+yargs.command({
+    command: 'remove',
+    describe: "Remove a new note",
+    handler: function () {
+        console.log("Removing the note");
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: "List all notes",
+    handler: function () {
+        console.log("List the notes");
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: "Read note",
+    handler: function () {
+        console.log("Read the note");
+    }
+})
+
+yargs.parse();
